95f9d88228fdb597b28dfdd3be543e2fa0c991aa
Walter Nedov <s4742274@student.uq.edu.au>
2025-12-19 17:04:18 +1000

    add tensorboard.

--- 04_DQN_Minh/Replaymem.py
+++ 04_DQN_Minh/Replaymem.py
@@ -5,14 +5,14 @@ Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'
 
 class ReplayMemory:
 
-    def __init__(self, capacity=10000000):
+    def __init__(self, capacity=1000000):
         self.mem = deque(maxlen=capacity)
     
     def push(self, *args): 
         self.mem.append(Transition(*args))
     
     def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
+        return random.sample(self.mem, batch_size)
 
     def __len__(self):
-        return len(self.memory)
+        return len(self.mem)
--- 04_DQN_Minh/main.py
+++ 04_DQN_Minh/main.py
@@ -4,28 +4,34 @@ import gymnasium as gym
 from gymnasium.wrappers import TransformReward, FrameStackObservation, AtariPreprocessing, TimeLimit
 import numpy as np
 import torch
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+import ale_py
 
 
 
 def create_env():
-    env = gym.make("ALE/Breakout-v5", render_mode="None")
+    env = gym.make("ALE/Breakout-v5", render_mode=None, frameskip=1)
     env = AtariPreprocessing(env, grayscale_obs=True, scale_obs=False, screen_size=84, frame_skip=4, terminal_on_life_loss=True)
     env = TransformReward(env, lambda r: np.sign(r))
-    env = FrameStackObservation(env, num_stack=4)
+    env = FrameStackObservation(env, stack_size=4)
     env = TimeLimit(env, max_episode_steps=4500)
     return env
 
 
-def train_network(policy_net, target_net, batch, loss_fn, optimizer, device):
+def train_network(policy_net, target_net, batch, loss_fn, optimizer, device, writer, steps_done):
     optimizer.zero_grad()
     policy_net.train()
     states, actions , next_states , rewards, terminations = zip(*batch)
 
-    states = torch.tensor(states, device=device)
-    terminations = torch.tensor(terminations, device=device)
-    actions = torch.tensor(actions, device=device).unsqueeze(1)
-    next_states = torch.tensor(next_states, device=device)
-    rewards = torch.tensor(rewards, device=device)
+    states = np.array(states) 
+    next_states = np.array(next_states)
+
+    states = torch.tensor(states, device=device, dtype=torch.float32)
+    terminations = torch.tensor(terminations, device=device, dtype=torch.float32)
+    actions = torch.tensor(actions, device=device, dtype=torch.int64).unsqueeze(1)
+    next_states = torch.tensor(next_states, device=device, dtype=torch.float32)
+    rewards = torch.tensor(rewards, device=device, dtype=torch.float32)
 
     pred = policy_net(states).gather(dim=1, index=actions).squeeze()
     future_rewards = target_net(next_states).max(dim=1)[0].detach()
@@ -35,12 +41,18 @@ def train_network(policy_net, target_net, batch, loss_fn, optimizer, device):
     loss.backward()
     optimizer.step()
 
+    if steps_done % 100 == 0: 
+        writer.add_scalar("Loss/train", loss.item(), steps_done)
 
 
-def train_episode(env, memory, policy_net, target_net, loss_fn, optimizer, steps_done, device):
+
+def train_episode(env, memory, policy_net, target_net, loss_fn, optimizer, steps_done, device, writer):
     episode_over = False
     state, info = env.reset()
+    state = np.array(state)
+    total_reward = 0;
     while not episode_over: 
+
         rand = np.random.rand()
         epsilon = max(0.1, 1 - 9e-7* steps_done)
         if rand < epsilon:  
@@ -48,11 +60,15 @@ def train_episode(env, memory, policy_net, target_net, loss_fn, optimizer, steps
         else:
             with torch.no_grad():
                 policy_net.eval()
-                actions = policy_net(torch.tensor(state, device=device).unsqueeze(0))
+                actions = policy_net(torch.tensor(state, device=device, dtype=torch.float32).unsqueeze(0))
                 action = torch.argmax(actions).item()
+
         next_state, reward, terminated, truncated, info = env.step(action)
+        next_state = np.array(next_state)
+        total_reward += reward 
         steps_done += 1
         episode_over = terminated or truncated
+
         memory.push(state, action, next_state, reward, episode_over)
         state = next_state
 
@@ -60,12 +76,15 @@ def train_episode(env, memory, policy_net, target_net, loss_fn, optimizer, steps
             continue
 
         batch = memory.sample(32)
-        train_network(policy_net, target_net, batch, loss_fn, optimizer, device)
+        train_network(policy_net, target_net, batch, loss_fn, optimizer, device, writer, steps_done)
 
         if steps_done % 10000 == 0:
             target_net.load_state_dict(policy_net.state_dict())
     
-    return steps_done
+    writer.add_scalar("Score/Episode_Reward", total_reward, steps_done)
+    writer.add_scalar("Epsilon", max(0.1, 1 - 9e-7 * steps_done), steps_done)
+    
+    return total_reward, steps_done
 
 
 
@@ -75,6 +94,8 @@ if __name__ == "__main__":
     env = create_env()
     memory = ReplayMemory(capacity=1000000)
     device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
+    writer = SummaryWriter("runs/breakout_v1")
 
     state_dim = env.observation_space.shape[0] 
     action_dim = env.action_space.n
@@ -91,5 +112,12 @@ if __name__ == "__main__":
     loss = torch.nn.MSELoss()
 
     steps_done = 0
-    for i in range(5000):
-        steps_done = train_episode(env, memory, policy_net, target_net, loss, optimizer, steps_done, device)
+    progress_bar = tqdm(range(5000), unit="episode", desc="Training")
+    for i in progress_bar:
+        total_reward, steps_done = train_episode(env, memory, policy_net, target_net, loss, optimizer, steps_done, device, writer)
+        progress_bar.set_postfix(
+            step=steps_done, 
+            score=total_reward 
+        )
+
+    torch.save(policy_net.state_dict(), "breakout_weights.pth")
